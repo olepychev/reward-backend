0c6aaba1edb8be837b8c0bbbc7c524639447d585
bluesky0707 <asmakech115@gmail.com>
2022-09-30 21:53:46 +0900

    remove cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import credit_route from './api/credit.route.js'
 
 
 const app = express()
 const port = 8000
 
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({
     extended: true
